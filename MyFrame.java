1b04104dc14c14c0dea2b5f9365acb6e0e8db0b6
Liam Walsh <liammail06@gmail.com>
2013-10-09 16:45:51 +0100

    Update myframe

--- MyFrame.java
+++ MyFrame.java
@@ -20,14 +20,21 @@ public class MyFrame extends JFrame {
 	{
 		super();
 		
+		//set up the frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("My Frame");
 		this.setSize(300, 300);
 		this.setLayout(new BorderLayout());
 		
+		//create the main panel and give it a text area
+		mainPanel = new JPanel();
+		textArea = new JTextArea("Text will be here");
+		mainPanel.add(textArea);
+		
+		
+		//create the buttons and add action listeners to them
 		b1 = new JButton("Click Left");
 		b2 = new JButton("Click Right");
-		
 		
 		//do button adding to panel and panel to frame first
 		b1.addActionListener(new ActionListener() {
@@ -49,15 +56,14 @@ public class MyFrame extends JFrame {
             }
         }); 
 		
-		mainPanel = new JPanel();
-		textArea = new JTextArea("Text will be here");
-		mainPanel.add(textArea);
 		
+		//create button panel and add the buttons to it
 		buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(1,2));
 		buttonPanel.add(b1);
 		buttonPanel.add(b2);
 		
+		//add panels to frame
 		this.add(buttonPanel, BorderLayout.SOUTH);
 		this.add(mainPanel, BorderLayout.CENTER);
 		this.setVisible(true);
